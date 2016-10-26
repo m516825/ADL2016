f589f4d0ba0c0b36633822ed8822c227b0eb8d03
m516825 <b01902078@ntu.edu.tw>
2016-10-26 09:23:00 +0800

    fix readme file

--- hw1/README.md
+++ hw1/README.md
@@ -7,19 +7,14 @@ before using the following instructions.
 
 Splited version of these two corpus(smaller) have been placed in ./corpus for runing testing
 
-* glove
+* glove and word2vec
 ```
-> chmod a+x run_glove.sh
-> ./run_glove.sh
-```
-* word2vec
-```
-> chmod a+x run_word2vec.sh
-> ./run_word2vec.sh
+> chmod a+x run_phase1.sh
+> ./run_phase1.sh $corpus $output_folder
 ```
 
 * ptt
 ```
-> chmod a+x run_ptt.sh
-> ./run_ptt.sh
+> chmod a+x run_phase2.sh
+> ./run_phase2.sh $corpus $output_folder
 ```
--- hw1/run_phase2.sh
+++ hw1/run_phase2.sh
@@ -3,11 +3,11 @@ if [[ ! -e $2 ]]; then
 	mkdir $2
 fi
 echo "start training ptt vector"
-python ./src/ptt_glove/vocab_ch.py --corpus $1
-python ./src/ptt_glove/corpus_ch.py --corpus $1 --corpus_out ./ptt_corpus_title.txt
-python ./src/ptt_glove/cooccur_ch.py 
-python ./src/ptt_glove/glove_ch.py --vector $2ptt_vector.txt
-python ./src/filterVocab/filterVocab.py ./src/filterVocab/fullVocab_phase2.txt < $2ptt_vector.txt > $2filter_vec.txt
+python2.7 ./src/ptt_glove/vocab_ch.py --corpus $1
+python2.7 ./src/ptt_glove/corpus_ch.py --corpus $1 --corpus_out ./ptt_corpus_title.txt
+python2.7 ./src/ptt_glove/cooccur_ch.py 
+python2.7 ./src/ptt_glove/glove_ch.py --vector $2ptt_vector.txt
+python2.7 ./src/filterVocab/filterVocab.py ./src/filterVocab/fullVocab_phase2.txt < $2ptt_vector.txt > $2filter_vec.txt
 if [[ -f ./ptt_corpus_title.txt ]]; then
 	rm ./ptt_corpus_title.txt
 fi
